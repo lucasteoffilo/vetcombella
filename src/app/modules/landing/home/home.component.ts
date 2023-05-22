422564beed14894a7c009c0056a39cf48f1db82f
Lucas Teofilo <lucasteoffilo@icloud.com>
2023-05-21 23:12:35 -0300

    wip sec 1

--- src/app/modules/landing/home/home.component.ts
+++ src/app/modules/landing/home/home.component.ts
@@ -1,16 +1,50 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 
 @Component({
-    selector     : 'landing-home',
-    templateUrl  : './home.component.html',
+    selector: 'landing-home',
+    templateUrl: './home.component.html',
     encapsulation: ViewEncapsulation.None
 })
-export class LandingHomeComponent
-{
+export class LandingHomeComponent implements OnInit, OnDestroy {
+
+    private _unsubscribeAll: Subject<any> = new Subject();
+
     /**
      * Constructor
      */
-    constructor()
-    {
+    constructor() {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+	
+    ngOnInit(): void {
+
+    }
+	
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Track by function for ngFor loops
+     *
+     * @param index
+     * @param item
+     */
+    trackByFn(index: number, item: any): any {
+        return item.id || index;
     }
 }
--- src/app/modules/landing/home/home.module.ts
+++ src/app/modules/landing/home/home.module.ts
@@ -1,29 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { SharedModule } from 'app/shared/shared.module';
 import { LandingHomeComponent } from 'app/modules/landing/home/home.component';
 import { landingHomeRoutes } from 'app/modules/landing/home/home.routing';
-import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 @NgModule({
     declarations: [
         LandingHomeComponent
     ],
-    imports     : [
+    imports: [
         RouterModule.forChild(landingHomeRoutes),
         MatButtonModule,
         MatIconModule,
         SharedModule,
-        MatButtonModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatIconModule,
         MatInputModule,
+        MatFormFieldModule,
+        MatExpansionModule
     ]
 })
-export class LandingHomeModule
-{
+export class LandingHomeModule {
 }
